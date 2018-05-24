c79bb66ab2f230321a3d71a9b9d1b4fe2621f46d
Chris Beser <beserchris@chriss-macbook-pro-3.local>
2018-05-24 07:24:36 -0700

    Initial Commit

---
+++ ChatApp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ChatApp
+//
+//  Created by Chris Beser on 5/24/18.
+//  Copyright © 2018 Chris Beser. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
